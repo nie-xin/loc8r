02a53628279e23e2d65005979a477f0ab03b0929
xnie <xin@slimpay.com>
2016-07-13 12:48:18 +0200

    Add code quality control

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,8 +1,8 @@
-var express = require('express');
-var router = express.Router();
-var controllerMain = require('../controllers/main');
+var express = require('express')
+var router = express.Router()
+var controllerMain = require('../controllers/main')
 
 /* GET home page. */
-router.get('/', controllerMain.index);
+router.get('/', controllerMain.index)
 
-module.exports = router;
+module.exports = router
