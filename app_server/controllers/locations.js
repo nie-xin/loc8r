3bf0d0052d0ef04c20272a8127fadf17e06f1f5b
xnie <xin@slimpay.com>
2016-07-15 11:12:00 +0200

    Add location-info template

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,11 +1,11 @@
 /* GET home page */
 module.exports.homelist = function (req, res) {
-  res.render('locations-list', { title: 'Home' })
+  res.render('location-list', { title: 'Home' })
 }
 
 /* GET location info page */
 module.exports.locationInfo = function (req, res) {
-  res.render('index', { title: 'Location info' })
+  res.render('location-info', { title: 'Location info' })
 }
 
 /* GET add review page */
