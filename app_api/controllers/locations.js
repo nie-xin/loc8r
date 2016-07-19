efdf1a8b99cc911fe2f33cf70ba7b3c3b5fc849f
xnie <xin@slimpay.com>
2016-07-19 16:57:38 +0200

    Add doc update

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -94,7 +94,7 @@ module.exports.locationsReadOne = function (req, res) {
       .findById(req.params.locationId)
       .exec(function (err, location) {
         if (!location) {
-          sendJsonResponse(res, 404, { message: 'locationdId not fond' })
+          sendJsonResponse(res, 404, { message: 'locationId not fond' })
           return
         } else if (err) {
           sendJsonResponse(res, 404, err)
@@ -107,6 +107,48 @@ module.exports.locationsReadOne = function (req, res) {
   }
 }
 
-module.exports.locationsUpdateOne = function (req, res) {}
+module.exports.locationsUpdateOne = function (req, res) {
+  if (!req.params.locationdId) {
+    sendJsonResponse(res, 404, { message: 'Not found, locationId is required' })
+    return
+  }
+
+  Loc
+    .findById(req.params.locationId)
+    .select('-reviews -rating')
+    .exec(function (err, location) {
+      if (!location) {
+        sendJsonResponse(res, 404, { message: 'locationdId not found' })
+        return
+      } else if (err) {
+        sendJsonResponse(res, 400, err)
+        return
+      }
+
+      location.name = req.body.name
+      location.address = req.body.address
+      location.facilities = req.body.facilities.split(',')
+      location.coords = [parseFloat(req.body.lng), parseFloat(req.body.lat)]
+      location.openingTimes = [{
+        days: req.body.days1,
+        opening: req.body.opening1,
+        closing: req.body.closing1,
+        closed: req.body.closed1
+      }, {
+        days: req.body.days2,
+        opening: req.body.opening2,
+        closing: req.body.closing2,
+        closed: req.body.closed2
+      }]
+
+      location.save(function (err, location) {
+        if (err) {
+          sendJsonResponse(res, 404, err)
+        } else {
+          sendJsonResponse(res, 200, location)
+        }
+      })
+    })
+}
 
 module.exports.locationsDeleteOne = function (req, res) {}
