2b61866a973867b2d781b8d55301a80ae7d4a0bf
xnie <xin@slimpay.com>
2016-07-19 14:59:37 +0200

    Add distance query

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -1,12 +1,66 @@
 var mongoose = require('mongoose')
 var Loc = mongoose.model('Location')
 
+var theEarth = (function () {
+  var earthRadius = 6371
+
+  var getDistanceFromRads = function (rads) {
+    return parseFloat(rads * earthRadius)
+  }
+
+  var getRadsFromDistance = function (distance) {
+    return parseFloat(distance / earthRadius)
+  }
+
+  return {
+    getDistanceFromRads: getDistanceFromRads,
+    getRadsFromDistance: getRadsFromDistance
+  }
+})()
+
 var sendJsonResponse = function (res, status, content) {
   res.status(status)
   res.json(content)
 }
 
-module.exports.locationsListByDistance = function (req, res) {}
+module.exports.locationsListByDistance = function (req, res) {
+  var lng = parseFloat(req.query.lng)
+  var lat = parseFloat(req.query.lat)
+
+  if (!lng || !lat) {
+    sendJsonResponse(res, 404, { message: 'lng and lat query parameters are required' })
+    return
+  }
+
+  var point = {
+    type: 'Point',
+    coordinates: [lng, lat]
+  }
+  var geoOptions = {
+    spheraical: true,
+    maxDistance: theEarth.getRadsFromDistance(20),
+    num: 10
+  }
+
+  Loc.geoNear(point, geoOptions, function (err, results, stats) {
+    var locations = []
+    if (err) {
+      sendJsonResponse(res, 404, err)
+    } else {
+      results.forEach(function (doc) {
+        locations.push({
+          distance: theEarth.getDistanceFromRads(doc.dis),
+          name: doc.obj.name,
+          address: doc.obj.address,
+          rating: doc.obj.rating,
+          facilities: doc.obj.facilities,
+          _id: doc.obj._id
+        })
+      })
+      sendJsonResponse(res, 200, locations)
+    }
+  })
+}
 
 module.exports.locationsCreate = function (req, res) {
   sendJsonResponse(res, 200, { status: 'success' })
