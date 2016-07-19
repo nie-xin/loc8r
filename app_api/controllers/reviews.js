68596f2d31d433239a1954369a16f09576cd5e9c
xnie <xin@slimpay.com>
2016-07-19 16:45:39 +0200

    Add sub doc creation

--- app_api/controllers/reviews.js
+++ app_api/controllers/reviews.js
@@ -6,7 +6,80 @@ var sendJsonResponse = function (res, status, content) {
   res.json(content)
 }
 
-module.exports.reviewsCreate = function (req, res) {}
+var doSetAverageRating = function (location) {
+  var i = null
+  var reviewCount = 0
+  var ratingAverage = 0
+  var ratingTotal = 0
+
+  if (location.reviews && location.reviews.length > 0) {
+    reviewCount = location.reviews.length
+    ratingTotal = 0
+    for (i = 0; i < reviewCount; i++) {
+      ratingTotal = ratingTotal + location.reviews[i].rating
+    }
+    ratingAverage = parseInt(ratingTotal / reviewCount, 10)
+    location.rating = ratingAverage
+    location.save(function (err) {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log('Average rating updated  to ', ratingAverage)
+      }
+    })
+  }
+}
+
+var updateAverageRating = function (locationId) {
+  Loc
+    .findById(locationId)
+    .select('rating reviews')
+    .exec(function (err, location) {
+      if (!err) {
+        doSetAverageRating(location)
+      }
+    })
+}
+
+var doAddReview = function (req, res, location) {
+  if (!location) {
+    sendJsonResponse(res, 404, { message: 'locationId not found' })
+  } else {
+    location.reviews.push({
+      author: req.body.author,
+      rating: req.body.rating,
+      reviewText: req.body.reviewText
+    })
+
+    location.save(function (err, location) {
+      var thisReview = null
+      if (err) {
+        sendJsonResponse(res, 400, err)
+      } else {
+        updateAverageRating(location._id)
+        thisReview = location.reviews[location.reviews.length - 1]
+        sendJsonResponse(res, 201, thisReview)
+      }
+    })
+  }
+}
+module.exports.reviewsCreate = function (req, res) {
+  var locationId = req.params.locationId
+  if (locationId) {
+    Loc
+      .findById(locationId)
+      .select('reviews')
+      .exec(function (err, location) {
+        if (err) {
+          sendJsonResponse(res, 400, err)
+        } else {
+          doAddReview(req, res, location)
+        }
+      })
+  } else {
+    sendJsonResponse(res, 404, { message: 'Not found, locationId required' })
+  }
+}
 
 module.exports.reviewsReadOne = function (req, res) {
   if (req.params && req.params.locationId && req.params.reviewId) {
@@ -19,7 +92,7 @@ module.exports.reviewsReadOne = function (req, res) {
         var review = null
 
         if (!location) {
-          sendJsonResponse(res, 404, { message: 'locationdId not fond' })
+          sendJsonResponse(res, 404, { message: 'locationId not fond' })
           return
         } else if (err) {
           sendJsonResponse(res, 404, err)
